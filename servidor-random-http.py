f73d28c61ecbca3b3f54910c8edf661b627569ff
Rodrigo Perela Posada <rperela@alpha29.aulas.gsyc.urjc.es>
2018-02-08 16:38:07 +0100

    añado ejercicio

---
+++ servidor-random-http.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import socket
+import random
+
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+mySocket.bind(('localhost', 1234))
+mySocket.listen(5)
+
+try:
+    while True:
+        print('Waiting for connections')
+        (recvSocket, address) = mySocket.accept()
+        aleatorio=str(random.randint(0, 999999999))
+        print('Request received:')
+        print(recvSocket.recv(2048))
+        print('Answering back...')
+        recvSocket.send(bytes("HTTP/1.1 200 OK\r\n\r\n" +
+                        "<html><body><h2>Hola. "  +
+                        "<a href=" + aleatorio + ">Dame otra</a>" +
+                        "</h2></body></html>" +
+                        "\r\n", "utf-8"))
+        recvSocket.close()
+except KeyboardInterrupt:
+    print("Closing binded socket")
+    mySocket.close()
